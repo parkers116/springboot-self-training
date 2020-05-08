f888219a4a9f28468e5b66b91cd6bd1b118856f7
Parker Suen <Parker Suen@192.168.30.101>
2020-05-08 12:07:40 +0800

    first commit

---
+++ springboot-demo/src/main/java/com/example/demo/SpringbootHelloWorld.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+//@Controller
+public class SpringbootHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@RequestMapping("/")
+	public String hello(){
+		return "Hello World!";
+	}
+	
+//	@GetMapping("/x")
+//	public String hello(){
+//		return "Hello World!";
+//	}
+//	
+//	@GetMapping("/index")
+//	public String index() {
+//		return "index";
+//	}
+
+}
